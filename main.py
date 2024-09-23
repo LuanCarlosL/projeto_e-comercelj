becd801c09a8ee1b53327fe4d834c9e580424aa1
LuanCarlosL <luancarloslaurentino52@gmail.com>
2024-09-22 22:56:31 -0300

    alt 6

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request, flash
-
+import json
 app  = Flask(__name__)
 app.config['SECRET_KEY'] = '060506'
 #rota do formulario
@@ -7,17 +7,32 @@ app.config['SECRET_KEY'] = '060506'
 @app.route('/')
 def home():
     return render_template('login.html')
+@app.route('/adm')
+
+
+
+
+
 @app.route('/login', methods =[ 'POST'])
 def login():
-    #dados que vou salvar do login
+    
     nome = request.form.get('nome')
-    #nome que vitor escolher
     senha= request.form.get('senha')
-    if (nome =='luan' and senha =='1234') or( nome =="victor" and senha =='1234'):
-        return render_template("usuario.html")
-    else:
-        flash("USUARIO INVALIDO")
-        return redirect("/")
+
+    with open('usuarios.json') as usuarios_cadastrados:
+        usuarios = json.load(usuarios_cadastrados)
+        cont = 0
+        for usuario in usuarios:
+            cont += 1
+            if nome == "luanadm" and senha == "1234":
+                return render_template('administrador.html')
+            if usuario['nome'] == nome and usuario['senha'] == senha:
+                return render_template('usuario.html')
+            
+            if cont >= (len(usuarios)):
+                flash('USUARIO INVALIDO')
+                return redirect("/")
+   
 
 
 
